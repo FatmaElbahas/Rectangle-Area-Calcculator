a0cb1ae00be9d0cf3cf6f0ffeb7b202b143750f5
PASSWOED <PASSWOED@DESKTOP-UI80RJC>
2024-12-07 23:37:23 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp108
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter The Width of the Rectangle");
+          Double width=Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter The Length of The Rectangle");
+            Double Length = Convert.ToDouble(Console.ReadLine());
+            Double Area = Length * width;
+            Console.WriteLine("Area of The Rectangle : " + Area); 
+
+        }
+    }
+}
